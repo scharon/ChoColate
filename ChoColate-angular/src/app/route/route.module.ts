59f2ff758541125dbb9123a38b2748d6d8817399
scharon <jolyscharon@gmail.com>
2017-02-14 15:38:55 +0100

    update route

--- ChoColate-angular/src/app/route/route.module.ts
+++ ChoColate-angular/src/app/route/route.module.ts
@@ -1,18 +1,20 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from '../index/index.component';
-import { ProfilUpdateComponent } from '../profil-lupdate/profil-lupdate.component';
+import { ProfilAendernComponent } from '../profil-aendern/profil-aendern.component';
 import { ProfileLoeschenComponent } from '../profile-loeschen/profile-loeschen.component';
 import { LoginComponent } from '../login/login.component';
 import { CheckKlasseComponent } from '../check-klasse/check-klasse.component';
+import { AendernPasswortComponent } from '../aendern-passwort/aendern-passwort.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'index',  component: IndexComponent  },
   { path: 'login',  component: LoginComponent  },
-  { path: 'profileUpdate', component: ProfilUpdateComponent },
+  { path: 'profileUpdate', component: ProfilAendernComponent },
   { path: 'profileLoeschen', component: ProfileLoeschenComponent },
-  { path: 'check-klasse', component: CheckKlasseComponent }
+  { path: 'check-klasse', component: CheckKlasseComponent },
+  { path: 'password-change', component: AendernPasswortComponent }
 ];
 
 @NgModule({
