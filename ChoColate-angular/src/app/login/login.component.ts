4c62a1ff81f859bc840fbb0e3144382881ab3669
scharon <jolyscharon@gmail.com>
2017-02-18 13:16:38 +0100

    small update

--- ChoColate-angular/src/app/login/login.component.ts
+++ ChoColate-angular/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Input, ViewChild} from '@angular/core';
-import {Validator} from '@angular/forms';
+import {Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +10,39 @@ export class LoginComponent implements OnInit {
 
   @Input() email: String;
   @Input() password: String;
+
+  form: FormGroup;
+  emailInput: FormControl;
+  passwordInput: FormControl;
+  private passwordValidator = [
+    Validators.required,
+    Validators.minLength(6)
+  ];
+  private emailValidator = [
+    Validators.required,
+    Validators.minLength(4)
+  ];
+
   beschreibung: String = `Aus Sicherheitsgründen muss das Passwort mindestens 7-stellig sein sowie einen
                 Groß-buchstaben, eine Ziffer und ein Sonderzeichen enthalten`;
   version: String = `edition 2016 dark night blue 1.0`;
 
-  constructor(){}
+  constructor(private formBuilder: FormBuilder){
+    this.formBuilder = formBuilder;
+    this.initFormular();
+  }
 
   ngOnInit() {}
+
+  initFormular() {
+    this.emailInput = new FormControl('', this.emailValidator );
+    this.passwordInput = new FormControl('', this.passwordValidator );
+
+    this.form = this.formBuilder.group({
+      email : this.emailInput,
+      password : this.passwordInput
+    });
+  }
 
   checkPassword( input: String){
     let reg : any = '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}';
@@ -35,7 +61,3 @@ export class LoginComponent implements OnInit {
 
 }
 
-export class LoginModel {
-  username: String;
-  password: String;
-}
