63def8a448da4a69dbf1091e971afef4de4dfe36
scharon <jolyscharon@gmail.com>
2017-02-23 21:01:16 +0100

    clean up

--- ChoColate-angular/src/app/route/route.module.ts
+++ ChoColate-angular/src/app/route/route.module.ts
@@ -7,7 +7,6 @@ import { ProfilLoeschenComponent } from '../profil-loeschen/profil-loeschen.comp
 import { LoginComponent } from '../login/login.component';
 import { CheckKlasseComponent } from '../check-klasse/check-klasse.component';
 import { AendernPasswortComponent } from '../aendern-passwort/aendern-passwort.component';
-import { CheckSchuleComponent } from '../check-schule/check-schule.component';
 import { CheckpageComponent } from '../checkpage/checkpage.component';
 import { TextbubbleComponent } from '../textbubble/textbubble.component';
 
@@ -24,12 +23,10 @@ const routes: Routes = [
     ]
   },
 
-
   { path: 'profileUpdate', component: ProfilAendernComponent },
   { path: 'profileLoeschen', component: ProfilLoeschenComponent },
   { path: 'check-klasse', component: CheckKlasseComponent },
   { path: 'password-change', component: AendernPasswortComponent },
-  { path: 'check-schule' , component: CheckSchuleComponent},
   { path: 'checkpage', component: CheckpageComponent},
   { path: 'text', component: TextbubbleComponent}
 ];
